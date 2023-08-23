a63fde6c435bcaf70d44fd88651071539f72467b
Toby16 <bolajibaasit14@gmail.com>
2023-08-22 22:48:16 -0400

    Basic dictionary | Report: Included documentation for child class and all methods belonging to the child class

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,16 +6,35 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """
+    a class that inherits from 'BaseCaching' class
+    and is a caching system
+    """
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
+        """
+        A method to assign to the dictionary 'self.cache_data'
+        the item value for the key 'key'
+
+        Args:
+            key - The key for an item
+            item - The item belonging to a key
+        """
         if (key is None) or (item is None):
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        A methof to return the value
+        in 'self.cache_data' linked to a key
+
+        Args:
+            key - The key for an item in 'self.cache_data'
+        """
         try:
             if key is None:
                 return None
