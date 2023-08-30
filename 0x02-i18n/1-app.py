1fda05dbe16587adcc9706aff9f46e614bc0e8b4
Toby16 <bolajibaasit14@gmail.com>
2023-08-29 21:12:48 -0400

    Basic Babel setup | Report: made corrections

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -15,16 +15,19 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-    app = Flask(__name__)
-    babel = Babel(app)
 
-    @app.route("/", strict_slashes=False)
-    def index() -> str:
-        """
-        view function to render '1-index.html'
-        """
-        return render_template("1-index.html")
+app = Flask(__name__)
+config = app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """
+    view function to render '1-index.html'
+    """
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    Config.app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True, port=5000, host="0.0.0.0")
