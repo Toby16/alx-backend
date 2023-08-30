55db0bd82ddf548822043ccccc5e73ce4b23a680
Toby16 <bolajibaasit14@gmail.com>
2023-08-29 21:42:15 -0400

    Mock logging in | Report: fixed IndentationError

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -59,7 +59,7 @@ def get_locale() -> str:
     locale = request.args.get('locale', '')
     if locale in app.config["LANGUAGES"]:
         return locale
-     return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/", strict_slashes=False)
