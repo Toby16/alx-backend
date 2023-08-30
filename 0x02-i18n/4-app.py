aad6194552e12295a205d3a956d3dd48dee19abf
Toby16 <bolajibaasit14@gmail.com>
2023-08-29 21:28:52 -0400

    Force locale with URL parameter | Report: Task Done

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Force locale with URL parameter
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    class to configure available languages in the app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+config = app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Retrieves the locale for a web page
+    """
+    queries = request.query_string.decode("utf-8").split("&")
+    query_table = dict(map(
+        lambda x: (x if "=" in x else "{}=".format(x)).split("="),
+        queries,
+    ))
+    if "locale" in query_table:
+        if query_table["locale"] in app.config["LANGUAGES"]:
+            return query_table["locale"]
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """
+    view function to render '4-index.html'
+    """
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
