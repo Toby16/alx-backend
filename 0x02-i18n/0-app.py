4a7eb5d2f871e72820d022bd9f022b8d2e01c5a2
Toby16 <bolajibaasit14@gmail.com>
2023-08-29 20:52:10 -0400

    Basic Flask app | Report: Function is type-annoted

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,10 +9,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """
+    view function to render 'index.html'
+    """
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host='0.0.0.0')
